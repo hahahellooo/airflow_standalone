35308a6e02766fb983f70f31bc848ab749cfc6af
hahahellooo <hahahello777@gmail.com>
2024-07-19 17:46:08 +0900

    2tmp

---
+++ dags/tmp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CSV_PATH=$1
+
+user="root"
+password="qwer123"
+database="cmd_history"
+
+mysql -u"$user" -p"$password" "$database" <<EOF
+-- LOAD DATA INFILE '/var/lib/mysql-files/csv.csv'
+LOAD DATA INFILE '${CSV_PATH}'
+INTO TABLE cmd_history.cmd_usage
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n';
+EOF
